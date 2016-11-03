e78b9977a33300e52c9a74da66ae2abcd04a153f
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-11-02 20:43:59 -0700

    complete basic styling for feed header

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Auth from './auth/auth';
 
 const App = (props) => (
-  <div className="main">
+  <main>
     {props.children}
     <footer className="footer">This is the footer that you will only see when logged out</footer>
-  </div>
+  </main>
 );
 
 export default App;
--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -12,23 +12,35 @@ class Feed extends React.Component {
 
     return(
       <div className="feed-page">
-        <div className="header">
-          <nav>
-            <div>
-              <a href="/" id="logo-link">
-                <img id="logo-link" src="http://res.cloudinary.com/skdkim/image/upload/v1478135475/nav-bar-logo_e04zmb.png"/>
+        <header>
+          <div>
+            <a href="/">
+              <img id="logo-link" src="http://res.cloudinary.com/skdkim/image/upload/v1478135475/nav-bar-logo_e04zmb.png"/>
+            </a>
+            <a href="/" id="hikerGram-text">
+              HikerGram
+            </a>
+          </div>
+          <div>
+            <input id="search-input" placeholder="search"></input>
+          </div>
+          <div>
+            <nav>
+              <a href="/" id="discover-link" >
+                <img className="small-icon" src="http://res.cloudinary.com/skdkim/image/upload/v1478143104/discover-icon_g8qioz.png"/>
               </a>
-            </div>
-            <div>
-              <input placeholder="search"></input>
-            </div>
-            <div>
-              <h4>3 icons</h4>
-            </div>
-          </nav>
-        </div>
-        <h2>Hello! {this.props.user.username}</h2>
-        <button onClick={this.handleLogout.bind(this)}>Log Out</button>
+              <a href="/" id="profile-link" >
+                <img className="small-icon" src="http://res.cloudinary.com/skdkim/image/upload/v1478143104/profile-icon_w41e7q.png"/>
+              </a>
+              <a href="/" id="logout-link" >
+                <img onClick={this.handleLogout.bind(this)} className="small-icon" src="http://res.cloudinary.com/skdkim/image/upload/v1478143104/logout-icon_zh94cc.png"/>
+              </a>
+            </nav>
+          </div>
+        </header>
+        <main className="feed border-top">
+          <h1>Where the feed goes</h1>
+        </main>
       </div>
 
     );
