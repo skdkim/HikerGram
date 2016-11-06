c6d8c7d61ad6d6bb9922d11c433b2376a979c35b
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-11-05 20:04:21 -0700

    add deflater

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Instagram
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.use Rack::Deflater
   end
 end
