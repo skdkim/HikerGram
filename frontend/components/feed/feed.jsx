b4be2d4d33e7fedb0acedb28bbecbfb48befb59e
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-11-07 10:35:44 -0800

    make feed photo capsule component

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+
 import HeaderContainer from '../header/header_container';
+
+import FeedCapsule from './feed_capsule';
 
 class Feed extends React.Component {
   render(){
     const currentUser = this.props.currentUser;
+
+    //take article out and make it into a functional component
 
     return(
       <div className="main-container">
@@ -12,46 +17,10 @@ class Feed extends React.Component {
         <main>
           <div className="feed border-top">
             <section className="feed-page">
-              <article className="whole-feed-photo-container">
-                <header className="feed-photo-header">
-                  <a className="feed-photo-user-icon" href={"/"}>
-                    <img className="feed-photo-user-icon-image" src={`${currentUser.image_url}`}/>
-                  </a>
-                  <h1 className="feed-sbt">{currentUser.username}</h1>
-                </header>
-
-                <div className="feed-photo-container">
-                  <img className="feed-photo" src={`${currentUser.photos[1].image_url}`}/>
-                </div>
-
-                <div className="fp-info">
-                  <div className="fpi-likes">
-                    <span className="fpil-text">
-                      <span className="feed-sbt">49</span>
-                      <span className="feed-sbt nbt"> likes</span>
-                    </span>
-                  </div>
-                  <div className="fpi-description">
-                    <ul className="fpid-comments">
-                      <li className="fpid-comment">
-                        <h1 className="feed-sbt">{currentUser.username}
-                          <span className="feed-sbt nbt"> {currentUser.photos[1].description}</span>
-                        </h1>
-                      </li>
-                    </ul>
-                  </div>
-                  <section className="fpi-add-comments">
-                    <a className="like-button">
-                      <span className="openHeartSprite">like</span>
-                    </a>
-                    <form className="add-a-comment">
-                      <input type="text" className="aac-input feed-sbt nbt" placeholder="Add a comment…" value=""/>
-                    </form>
-                  </section>
-                </div>
-
-
-              </article>
+              // {
+              //   currentUser.photos.map((photo, key) => <FeedCapsule photo={photo} />)
+              // }
+              <FeedCapsule />
             </section>
           </div>
         </main>
---
+++ frontend/components/feed/feed_capsule.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const FeedCapsule = (props) => (
+  <article className="whole-feed-photo-container">
+    <header className="feed-photo-header">
+      <a className="feed-photo-user-icon" href={"/"}>
+        <img className="feed-photo-user-icon-image" src={`${currentUser.image_url}`}/>
+      </a>
+      <h1 className="feed-sbt">{currentUser.username}</h1>
+    </header>
+
+    <div className="feed-photo-container">
+      <img className="feed-photo" src={`${currentUser.photos[1].image_url}`}/>
+    </div>
+
+    <div className="fp-info">
+      <div className="fpi-likes">
+        <span className="fpil-text">
+          <span className="feed-sbt">49</span>
+          <span className="feed-sbt nbt"> likes</span>
+        </span>
+      </div>
+      <div className="fpi-description">
+        <ul className="fpid-comments">
+          <li className="fpid-comment">
+            <h1 className="feed-sbt">{currentUser.username}
+              <span className="feed-sbt nbt"> {currentUser.photos[1].description}</span>
+            </h1>
+          </li>
+        </ul>
+      </div>
+      <section className="fpi-add-comments">
+        <a className="like-button">
+          <span className="openHeartSprite">like</span>
+        </a>
+        <form className="add-a-comment">
+          <input type="text" className="aac-input feed-sbt nbt" placeholder="Add a comment…" value=""/>
+        </form>
+      </section>
+    </div>
+
+
+  </article>
+);
+
+export default FeedCapsule;
