86e7a9b871f496e6696cabc7b66e87934d6a10a0
Sung Kwang David Kim <sungkwangkim@gmail.com>
2016-11-12 16:02:23 -0800

    tweek sign in page styling

--- frontend/components/auth/auth.jsx
+++ frontend/components/auth/auth.jsx
@@ -63,23 +63,27 @@ class Auth extends React.Component {
   render(){
     return(
       <div className="splash">
-        <div className="auth-form">
-          <h3 className="form-welcome-title">Welcome to HikerGram</h3>
-          <form onSubmit={this.handleSubmit.bind(this)}>
-            <div className="inputs">
-              <label className="username-login"><input className="auth-inputs" onChange={this.handleUsername.bind(this)} value={this.state.username} placeholder={"Username"}/></label>
-              <label className="password-login"><input className="auth-inputs" onChange={this.handlePassword.bind(this)} type="password" value={this.state.password} placeholder={"Password"}/></label>
-            </div>
+        <div>
+          <div className="auth-form">
+            <h3 className="form-welcome-title">Welcome to HikerGram</h3>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <div className="inputs">
+                <label className="username-login"><input className="auth-inputs" onChange={this.handleUsername.bind(this)} value={this.state.username} placeholder={"Username"}/></label>
+                <label className="password-login"><input className="auth-inputs" onChange={this.handlePassword.bind(this)} type="password" value={this.state.password} placeholder={"Password"}/></label>
+              </div>
+              {this.renderErrors()}
 
-            <div className="station-buttons">
-              <button className="login" onClick={() => this.type = "login"}>Log In</button>
-              <button className="signup" onClick={() => this.type = "signup"}>Sign Up</button>
-            </div>
-            <div className="guest-button">
-              <button className="guest" onClick={() => this.type = "guest"}>Guest Login</button>
-            </div>
-            {this.renderErrors()}
-          </form>
+              <div className="all-buttons">
+                <div className="station-buttons">
+                  <button className="login" onClick={() => this.type = "login"}>Log In</button>
+                  <button className="signup" onClick={() => this.type = "signup"}>Sign Up</button>
+                </div>
+                <div className="guest-button">
+                  <button className="guest" onClick={() => this.type = "guest"}>Guest Login</button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
